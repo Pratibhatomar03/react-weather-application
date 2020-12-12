47b73972b85bcd7fd977d23eedd799f37cda30f8
Pratibha <pratibha.tomar13@gmail.com>
2020-12-12 15:39:33 +0100

    added link to github and portfolio

--- src/App.js
+++ src/App.js
@@ -6,7 +6,14 @@ export default function App() {
     <div className="App">
       <h1>Weather App</h1>
       <footer>
-        This poject is coded by Pratibha Tomar and is open source on GitHib
+        This poject is coded by{" "}
+        <a href="https://ecstatic-bassi-280461.netlify.app/" target="_blank">
+          Pratibha Tomar
+        </a>{" "}
+        and is open source on{" "}
+        <a href="https://peaceful-poitras-26321d.netlify.app/" target="_blank">
+          GitHib
+        </a>
       </footer>
     </div>
   );
